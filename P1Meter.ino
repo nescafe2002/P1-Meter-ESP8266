a23e20bc18cfec19a585eb079a5b5b7e621c4909
Joris <joris@talma.nl>
2018-08-19 19:39:15 +0200

    Emoncms + Sagemcom

--- P1Meter.ino
+++ P1Meter.ino
@@ -11,10 +11,10 @@
 const char* ssid = "WIFISSID";
 const char* password = "PASSWORD";
 const char* hostName = "ESPP1Meter";
-const char* domoticzIP = "192.168.1.35";
-const int domoticzPort = 8090;
-const int domoticzGasIdx = 291;
-const int domoticzEneryIdx = 294;
+const char* emonCmsHost = "emoncms.org";
+const int emonCmsPort = 80;
+const int emonCmsNode = 8266;
+const char* emonCmsApiKey = "";
 const bool outputOnSerial = true;
 //===Change values to here===
 
@@ -25,23 +25,28 @@ long mEOLT = 0; //Meter reading Electrics - return low tariff
 long mEOHT = 0; //Meter reading Electrics - return high tariff
 long mEAV = 0;  //Meter reading Electrics - Actual consumption
 long mEAT = 0;  //Meter reading Electrics - Actual return
+long mAV1 = 0; // Actueel vermogen L1
+long mAV2 = 0; // Actueel vermogen L2
+long mAV3 = 0; // Actueel vermogen L3
+long mAT1 = 0; // Actuele teruglevering L1
+long mAT2 = 0; // Actuele teruglevering L2
+long mAT3 = 0; // Actuele teruglevering L3
 long mGAS = 0;    //Meter reading Gas
 long prevGAS = 0;
 
+unsigned long mNextUpdateTime = 0;
+const unsigned long mUpdatePeriod = 10000;
 
-#define MAXLINELENGTH 64 // longest normal line is 47 char (+3 for \r\n\0)
+#define MAXLINELENGTH 1023 // long enough for whole update diagram
 char telegram[MAXLINELENGTH];
 
-#define SERIAL_RX     D5  // pin for SoftwareSerial RX
+char checksum[6]; // 4 chars + \r\n
+
+#define SERIAL_RX     14  // pin for SoftwareSerial RX: D5 = 14
 SoftwareSerial mySerial(SERIAL_RX, -1, true, MAXLINELENGTH); // (RX, TX. inverted, buffer)
 
-unsigned int currentCRC=0;
-
-void SendToDomoLog(char* message)
-{
-  char url[512];
-  sprintf(url, "http://%s:%d/json.htm?type=command&param=addlogmessage&message=%s", domoticzIP, domoticzPort, message); 
-}
+WiFiUDP g_udp;
+const int g_port = 8888;
 
 void setup() {
   Serial.begin(115200);
@@ -67,6 +72,7 @@ void setup() {
 
   ArduinoOTA.onStart([]() {
     Serial.println("Start");
+    mySerial.enableRx(false);
   });
   ArduinoOTA.onEnd([]() {
     Serial.println("\nEnd");
@@ -86,16 +92,30 @@ void setup() {
   Serial.println("Ready");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+  // start UDP server
+  g_udp.begin(g_port);
 }
 
 
-bool SendToDomo(int idx, int nValue, char* sValue)
+bool SendToDebug(char* sValue)
+{
+  IPAddress ip = WiFi.localIP();
+  ip[3] = 255;
+
+  // transmit broadcast package
+  g_udp.beginPacket(ip, g_port);
+  g_udp.write(sValue);
+  g_udp.endPacket();
+}
+
+
+bool SendToEmonCms(char* sValue)
 {
   HTTPClient http;
   bool retVal = false;
   char url[255];
-  sprintf(url, "http://%s:%d/json.htm?type=command&param=udevice&idx=%d&nvalue=%d&svalue=%s", domoticzIP, domoticzPort, idx, nValue, sValue);
-  Serial.printf("[HTTP] GET... URL: %s\n",url);
+  sprintf(url, "http://%s:%d/input/post.json?node=%d&apikey=%s&json=%s", emonCmsHost, emonCmsPort, emonCmsNode, emonCmsApiKey, sValue);
+  Serial.printf("[HTTP] GET... URL: %s\n", url);
   http.begin(url); //HTTP
   int httpCode = http.GET();
   // httpCode will be negative on error
@@ -125,8 +145,8 @@ void UpdateGas()
   if(prevGAS!=mGAS)
   {
     char sValue[10];
-    sprintf(sValue, "%d", mGAS);
-    if(SendToDomo(domoticzGasIdx, 0, sValue))
+    sprintf(sValue, "{GAS:%d}", mGAS);
+    if(SendToEmonCms(sValue))
       prevGAS=mGAS;
   }
 }
@@ -134,8 +154,8 @@ void UpdateGas()
 void UpdateElectricity()
 {
   char sValue[255];
-  sprintf(sValue, "%d;%d;%d;%d;%d;%d", mEVLT, mEVHT, mEOLT, mEOHT, mEAV, mEAT);
-  SendToDomo(domoticzEneryIdx, 0, sValue);
+  sprintf(sValue, "{EVLT:%d,EVHT:%d,EOLT:%d,EOHT:%d,EAV:%d,EAT:%d,AV1:%d,AV2:%d,AV3:%d,AT1:%d,AT2:%d,AT3:%d}", mEVLT, mEVHT, mEOLT, mEOHT, mEAV, mEAT, mAV1, mAV2, mAV3, mAT1, mAT2, mAT3);
+  SendToEmonCms(sValue);
 }
 
 
@@ -183,119 +203,100 @@ long getValue(char* buffer, int maxlen) {
   return 0;
 }
 
+bool verifyChecksum(int len) {
+
+  unsigned int crc = CRC16(0x0000, (unsigned char*)telegram, len);
+  return (strtol(checksum, NULL, 16) == crc);
+}
+
 bool decodeTelegram(int len) {
-  //need to check for start
-  int startChar = FindCharInArrayRev(telegram, '/', len);
-  int endChar = FindCharInArrayRev(telegram, '!', len);
-  bool validCRCFound = false;
-  if(startChar>=0)
-  {
-    //start found. Reset CRC calculation
     currentCRC=CRC16(0x0000,(unsigned char *) telegram+startChar, len-startChar);
-    if(outputOnSerial)
-    {
-      for(int cnt=startChar; cnt<len-startChar;cnt++)
-        Serial.print(telegram[cnt]);
-    }    
-    //Serial.println("Start found!");
-    
+
+  // Read each command pair
+  char* command = strtok(telegram, "\n");
+  while (command != 0) {
+    int cmdlen = strlen(command);
+
+    // 1-0:1.8.1(000992.992*kWh)
+    // 1-0:1.8.1 = Elektra verbruik laag tarief (DSMR v4.0)
+    if (strncmp(command, "1-0:1.8.1(", strlen("1-0:1.8.1(")) == 0)
+      mEVLT = getValue(command, cmdlen);
+
+    // 1-0:1.8.2(000560.157*kWh)
+    // 1-0:1.8.2 = Elektra verbruik hoog tarief (DSMR v4.0)
+    else if (strncmp(command, "1-0:1.8.2(", strlen("1-0:1.8.2(")) == 0)
+      mEVHT = getValue(command, cmdlen);
+
+    // 1-0:2.8.1(000348.890*kWh)
+    // 1-0:2.8.1 = Elektra opbrengst laag tarief (DSMR v4.0)
+    else if (strncmp(command, "1-0:2.8.1(", strlen("1-0:2.8.1(")) == 0)
+      mEOLT = getValue(command, cmdlen);
+
+    // 1-0:2.8.2(000859.885*kWh)
+    // 1-0:2.8.2 = Elektra opbrengst hoog tarief (DSMR v4.0)
+    else if (strncmp(command, "1-0:2.8.2(", strlen("1-0:2.8.2(")) == 0)
+      mEOHT = getValue(command, cmdlen);
+
+    // 1-0:1.7.0(00.424*kW) Actueel verbruik
+    // 1-0:2.7.0(00.000*kW) Actuele teruglevering
+    // 1-0:1.7.x = Electricity consumption actual usage (DSMR v4.0)
+    else if (strncmp(command, "1-0:1.7.0(", strlen("1-0:1.7.0(")) == 0)
+      mEAV = getValue(command, cmdlen);
+    else if (strncmp(command, "1-0:2.7.0(", strlen("1-0:2.7.0(")) == 0)
+      mEAT = getValue(command, cmdlen);
+
+    // Actueel vermogen L1, L2, L3
+    else if (strncmp(command, "1-0:21.7.0(", strlen("1-0:21.7.0(")) == 0)
+      mAV1 = getValue(command, cmdlen);
+    else if (strncmp(command, "1-0:41.7.0(", strlen("1-0:41.7.0(")) == 0)
+      mAV2 = getValue(command, cmdlen);
+    else if (strncmp(command, "1-0:61.7.0(", strlen("1-0:61.7.0(")) == 0)
+      mAV3 = getValue(command, cmdlen);
+
+    // Actuele teruglevering L1, L2, L3
+    else if (strncmp(command, "1-0:22.7.0(", strlen("1-0:22.7.0(")) == 0)
+      mAT1 = getValue(command, cmdlen);
+    else if (strncmp(command, "1-0:42.7.0(", strlen("1-0:42.7.0(")) == 0)
+      mAT2 = getValue(command, cmdlen);
+    else if (strncmp(command, "1-0:62.7.0(", strlen("1-0:62.7.0(")) == 0)
+      mAT3 = getValue(command, cmdlen);
+
+    // 0-1:24.2.1(150531200000S)(00811.923*m3)
+    // 0-1:24.2.1 = Gas (DSMR v4.0) on Kaifa MA105 meter
+    else if (strncmp(command, "0-1:24.2.1(", strlen("0-1:24.2.1(")) == 0)
+      mGAS = getValue(command, cmdlen);
+
+    // Find the next command in input string
+    command = strtok(0, "\n");
   }
-  else if(endChar>=0)
-  {
-    //add to crc calc 
-    currentCRC=CRC16(currentCRC,(unsigned char*)telegram+endChar, 1);
-    char messageCRC[5];
-    strncpy(messageCRC, telegram + endChar + 1, 4);
-    messageCRC[4]=0; //thanks to HarmOtten (issue 5)
-    if(outputOnSerial)
-    {
-      for(int cnt=0; cnt<len;cnt++)
-        Serial.print(telegram[cnt]);
-    }    
-    validCRCFound = (strtol(messageCRC, NULL, 16) == currentCRC);
-    if(validCRCFound)
-      Serial.println("\nVALID CRC FOUND!"); 
-    else
-      Serial.println("\n===INVALID CRC FOUND!===");
-    currentCRC = 0;
-  }
-  else
-  {
-    currentCRC=CRC16(currentCRC, (unsigned char*)telegram, len);
-    if(outputOnSerial)
-    {
-      for(int cnt=0; cnt<len;cnt++)
-        Serial.print(telegram[cnt]);
-    }
-  }
 
-  long val =0;
-  long val2=0;
-  // 1-0:1.8.1(000992.992*kWh)
-  // 1-0:1.8.1 = Elektra verbruik laag tarief (DSMR v4.0)
-  if (strncmp(telegram, "1-0:1.8.1", strlen("1-0:1.8.1")) == 0) 
-    mEVLT =  getValue(telegram, len);
-  
-
-  // 1-0:1.8.2(000560.157*kWh)
-  // 1-0:1.8.2 = Elektra verbruik hoog tarief (DSMR v4.0)
-  if (strncmp(telegram, "1-0:1.8.2", strlen("1-0:1.8.2")) == 0) 
-    mEVHT = getValue(telegram, len);
-    
-
-  // 1-0:2.8.1(000348.890*kWh)
-  // 1-0:2.8.1 = Elektra opbrengst laag tarief (DSMR v4.0)
-  if (strncmp(telegram, "1-0:2.8.1", strlen("1-0:2.8.1")) == 0) 
-    mEOLT = getValue(telegram, len);
-   
-
-  // 1-0:2.8.2(000859.885*kWh)
-  // 1-0:2.8.2 = Elektra opbrengst hoog tarief (DSMR v4.0)
-  if (strncmp(telegram, "1-0:2.8.2", strlen("1-0:2.8.2")) == 0) 
-    mEOHT = getValue(telegram, len);
-    
-
-  // 1-0:1.7.0(00.424*kW) Actueel verbruik
-  // 1-0:2.7.0(00.000*kW) Actuele teruglevering
-  // 1-0:1.7.x = Electricity consumption actual usage (DSMR v4.0)
-  if (strncmp(telegram, "1-0:1.7.0", strlen("1-0:1.7.0")) == 0) 
-    mEAV = getValue(telegram, len);
-    
-  if (strncmp(telegram, "1-0:2.7.0", strlen("1-0:2.7.0")) == 0)
-    mEAT = getValue(telegram, len);
-   
-
-  // 0-1:24.2.1(150531200000S)(00811.923*m3)
-  // 0-1:24.2.1 = Gas (DSMR v4.0) on Kaifa MA105 meter
-  if (strncmp(telegram, "0-1:24.2.1", strlen("0-1:24.2.1")) == 0) 
-    mGAS = getValue(telegram, len);
-
-  return validCRCFound;
+  // sanity check
+  return (abs(mEAV - mAV1 - mAV2 - mAV3) < 10 && abs(mEAT - mAT1 - mAT2 - mAT3) < 10);
 }
 
 void readTelegram() {
   if (mySerial.available()) {
-    memset(telegram, 0, sizeof(telegram));
-    while (mySerial.available()) {
-      int len = mySerial.readBytesUntil('\n', telegram, MAXLINELENGTH);
-      telegram[len] = '\n';
-      telegram[len+1] = 0;
-      yield();
-      if(decodeTelegram(len+1))
-      {
-         UpdateElectricity();
-         UpdateGas();
+
+    int len = mySerial.readBytesUntil('!', telegram, MAXLINELENGTH);
+    telegram[len] = '!';
+    telegram[len + 1] = 0;
+
+    if (mySerial.available()) {
+
+      mySerial.readBytesUntil('\n', checksum, 6);
+      checksum[4] = 0;
+
+      if (millis() > mNextUpdateTime && verifyChecksum(len + 1) && decodeTelegram(len + 1)) {
+          UpdateElectricity();
+          UpdateGas();
+          mNextUpdateTime = millis() + mUpdatePeriod;
       }
-    } 
+    }
   }
 }
-
-
 
 void loop() {
   readTelegram();
   ArduinoOTA.handle();
+  yield();
 }
-
-
-
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # P1-Meter-ESP8266
-Software for the ESP2866 that sends P1 smart meter data to Domoticz (with CRC checking and OTA firmware updates)
+Software for the ESP2866 that sends P1 smart meter data to Emoncms (with CRC checking and OTA firmware updates)
 
 ### Installation instrucions
 - Make sure that your ESP8266 can be flashed from the Arduino environnment: https://github.com/esp8266/Arduino
 - Install the SoftSerial library from: https://github.com/plerup/espsoftwareserial
 - Place all files from this repository in a directory. Open the .ino file.
-- Adjust WIFI, Domoticz and debug settings at the top of the file
+- Adjust WIFI, Emoncms and debug settings at the top of the file
 - Compile and flash
 
 ### Connection of the P1 meter to the ESP8266
@@ -13,3 +13,10 @@ You need to connect the smart meter with a RJ11 connector. This is the pinout to
 ![RJ11 P1 connetor](http://gejanssen.com/howto/Slimme-meter-uitlezen/RJ11-pinout.png)
 
 Connect GND->GND on ESP, RTS->3.3V on ESP and RxD->any digital pin on ESP. In this sketch I use D5
+
+### Changelog
+- Added support for three phase system (L1/L2/L3)
+- Added support for Sagemcom T210-D (softserial read in single command, update once every 10 seconds)
+- Improved OTA support (disable softserial rx when updating)
+- Added support for Emoncms
+- Added udp logger SendToDebug()
